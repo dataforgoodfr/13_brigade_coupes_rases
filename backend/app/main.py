44a1de1216543db0a2fd7e5649ef2c70133145f1
inimaz <93inigo93@gmail.com>
2025-02-18 09:08:29 +0100

    fix: use global poetry
    Signed-off-by: inimaz <93inigo93@gmail.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,11 @@ async def home():
     return {"message": "Hello, World!"}
 
 
-if __name__ == "__main__":
+def start_server():
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    start_server()
