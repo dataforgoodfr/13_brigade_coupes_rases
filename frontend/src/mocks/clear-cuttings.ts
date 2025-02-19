fbdfc01da19a87a4b101e51b61fd47c26af05c1e
Idriss <ihippocrate@gmail.com>
2025-02-19 19:37:06 +0100

    Generate random polygons for mock data

--- frontend/src/mocks/clear-cuttings.ts
+++ frontend/src/mocks/clear-cuttings.ts
@@ -1,72 +1,110 @@
 import type {
-	ClearCuttingPreview,
-	ClearCuttingsResponse,
+  ClearCuttingPreview,
+  ClearCuttingsResponse,
 } from "@/features/clear-cutting/store/clear-cuttings";
 import type { FiltersResponse } from "@/features/clear-cutting/store/filters";
 import { range } from "@/shared/array";
 import { faker } from "@faker-js/faker";
 import { http, HttpResponse } from "msw";
+
 export const mockFilters = http.get("*/filters", () => {
-	return HttpResponse.json({
-		cutYears: [2021, 2022],
-		tags: [
-			{
-				isAbusive: true,
-				name: "Supérieur à 10 hectares",
-				id: faker.string.uuid(),
-			},
-		],
-		ecologicalZoning: {
-			[faker.string.uuid()]: faker.company.buzzAdjective(),
-		},
-		status: {},
-		departments: {},
-		region: {},
-	} satisfies FiltersResponse);
+  return HttpResponse.json({
+    cutYears: [2021, 2022],
+    tags: [
+      {
+        isAbusive: true,
+        name: "Supérieur à 10 hectares",
+        id: faker.string.uuid(),
+      },
+    ],
+    ecologicalZoning: {
+      [faker.string.uuid()]: faker.company.buzzAdjective(),
+    },
+    status: {},
+    departments: {},
+    region: {},
+  } satisfies FiltersResponse);
 });
+
 export const mockClearCuttings = http.get("*/clear-cuttings", () => {
-	return HttpResponse.json({
-		clearCuttingPreviews: [
-			createClearCutting(),
-			createClearCutting(),
-			createClearCutting(),
-			createClearCutting(),
-			createClearCutting(),
-			createClearCutting(),
-			createClearCutting(),
-			createClearCutting(),
-		],
-		clearCuttingsPoints: createFranceRandomPoints,
-		ecologicalZoning: [],
-		waterCourses: [],
-	} satisfies ClearCuttingsResponse);
+  return HttpResponse.json({
+    clearCuttingPreviews: createFranceRandomPoints.map(createClearCutting),
+    clearCuttingsPoints: createFranceRandomPoints,
+    ecologicalZoning: [],
+    waterCourses: [],
+  } satisfies ClearCuttingsResponse);
 });
+
 const francRandomPointMock = (): [number, number] => [
-	faker.location.latitude({
-		min: 43.883918307385926,
-		max: 49.33292664908802,
-	}),
-	faker.location.longitude({
-		min: -0.3899356021470268,
-		max: 5.666435865557435,
-	}),
+  faker.location.latitude({
+    min: 43.883918307385926,
+    max: 49.33292664908802,
+  }),
+  faker.location.longitude({
+    min: -0.3899356021470268,
+    max: 5.666435865557435,
+  }),
 ];
+
+const randomPolygonFromLocation = (
+  point: [number, number],
+  radius = 10,
+  size = 10
+): [number, number][] => {
+  if (size < 3) {
+    throw new Error(
+      "Invalid polygon size: maximum polygon size can not be less than 3"
+    );
+  }
+
+  if (radius <= 0) {
+    throw new Error("Invalid radius size: must be grater to 0");
+  }
+
+  const earthRadius = 6371; // Earth radius
+  const polygon: [number, number][] = [];
+  const angleStep = (2 * Math.PI) / size;
+
+  for (let i = 0; i < size; i++) {
+    const angle = i * angleStep;
+
+    const deltaLat = (radius / earthRadius) * (180 / Math.PI) * Math.sin(angle);
+    const deltaLng =
+      ((radius / earthRadius) * (180 / Math.PI) * Math.cos(angle)) /
+      Math.cos((point[0] * Math.PI) / 180);
+
+    polygon.push([point[0] + deltaLat, point[1] + deltaLng]);
+  }
+
+  return polygon;
+};
+
 const createFranceRandomPoints = range<[number, number]>(
-	500,
-	francRandomPointMock,
+  500,
+  francRandomPointMock
 );
-const createClearCutting = (): ClearCuttingPreview => ({
-	center: francRandomPointMock(),
-	address: {
-		city: faker.location.city(),
-		country: faker.location.country(),
-		postalCode: faker.location.zipCode(),
-	},
-	status: "validated",
-	name: faker.animal.dog(),
-	cutYear: 2021,
-	id: faker.string.uuid(),
-	imagesCnt: faker.number.int() % 10,
-	imageUrl: faker.image.url(),
-	geoCoordinates: [],
-});
+
+const createClearCutting = (center: [number, number]): ClearCuttingPreview => {
+  const name = faker.animal.dog();
+  const city = faker.location.city();
+
+  return {
+    center,
+    name,
+    address: {
+      city,
+      country: faker.location.country(),
+      postalCode: faker.location.zipCode(),
+    },
+    status: "validated",
+    cutYear: 2021,
+    id: faker.string.uuid(),
+    imagesCnt: faker.number.int() % 10,
+    imageUrl: faker.image.url(),
+    geoCoordinates: randomPolygonFromLocation(
+      center,
+      city.length / 10,
+      name.length
+    ),
+  };
+};
