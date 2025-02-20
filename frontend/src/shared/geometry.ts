f1a5a780603d18f9c92ae7d5d54cac53caf9d972
Idriss <ihippocrate@gmail.com>
2025-02-20 18:35:20 +0100

    update code formatting

--- frontend/src/shared/geometry.ts
+++ frontend/src/shared/geometry.ts
@@ -1,26 +1,26 @@
 export type Boundaries = [
-  [number, number],
-  [number, number],
-  [number, number],
-  [number, number]
+	[number, number],
+	[number, number],
+	[number, number],
+	[number, number],
 ];
 export function isPointInsidePolygon(
-  boundaries: Boundaries,
-  point: [number, number]
+	boundaries: Boundaries,
+	point: [number, number],
 ) {
-  const [lng, lat] = point;
-  let inside = false;
+	const [lng, lat] = point;
+	let inside = false;
 
-  for (let i = 0, j = boundaries.length - 1; i < boundaries.length; j = i++) {
-    const [lngi, lati] = boundaries[i];
-    const [lngj, latj] = boundaries[j];
+	for (let i = 0, j = boundaries.length - 1; i < boundaries.length; j = i++) {
+		const [lngi, lati] = boundaries[i];
+		const [lngj, latj] = boundaries[j];
 
-    const intersect =
-      lati > lat !== latj > lat &&
-      lng < ((lngj - lngi) * (lat - lati)) / (latj - lati) + lngi;
+		const intersect =
+			lati > lat !== latj > lat &&
+			lng < ((lngj - lngi) * (lat - lati)) / (latj - lati) + lngi;
 
-    if (intersect) inside = !inside;
-  }
+		if (intersect) inside = !inside;
+	}
 
-  return inside;
+	return inside;
 }
