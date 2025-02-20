8c055de116d20e81feb76730729598cf5c8a4843
Esr0g <gorse.sacha@gmail.com>
2025-02-20 09:49:05 +0100

    refactor(routing): changes of how the map routing is handled

--- frontend/src/features/clear-cutting/components/Form.tsx
+++ frontend/src/features/clear-cutting/components/Form.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { AccordionFullItem } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { useMapInstance } from "@/features/clear-cutting/components/map/Map.context";
@@ -55,30 +54,3 @@ export function AsideForm({ clearCuttingId }: AsideFormProps) {
 		</>
 	);
 }
-=======
-import { Button } from "@/components/ui/button"
-import { useMapInstance } from "./map/Map.context"
-import { useNavigate } from "@tanstack/react-router"
-
-type AsideFormProps = {
-    clearCuttingId? : string,
-}
-
-export function AsideForm({ clearCuttingId } : AsideFormProps) {
-    const { map } = useMapInstance();
-    const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     if (map) {
-    //         map.flyTo(clearCuttingPreview.center, 10, { duration: 1});
-    //     }
-    // }, [map])
-
-    return (
-        <>
-            <Button onClick={() => navigate({ to: '/map/list' })}>Close</Button>
-            <div>Ceci est la fiche {clearCuttingId}</div>
-        </>
-    );
-}
->>>>>>> 92f8973 (refactor(map): Change in routes management)
