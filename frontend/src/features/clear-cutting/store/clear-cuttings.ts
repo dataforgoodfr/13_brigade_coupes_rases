010b808914441cfa7eed7686f952e8849d6bf64d
Idriss <ihippocrate@gmail.com>
2025-02-20 18:35:20 +0100

    update code formatting

--- frontend/src/features/clear-cutting/store/clear-cuttings.ts
+++ frontend/src/features/clear-cutting/store/clear-cuttings.ts
@@ -2,10 +2,10 @@ import { string, z } from "zod";
 import { pointSchema } from "./types";
 
 export const clearCuttingStatusSchema = z.enum([
-  "toValidate",
-  "validated",
-  "rejected",
-  "waitingInformation",
+	"toValidate",
+	"validated",
+	"rejected",
+	"waitingInformation",
 ]);
 
 export type ClearCuttingStatus = z.infer<typeof clearCuttingStatusSchema>;
@@ -13,67 +13,67 @@ export type ClearCuttingStatus = z.infer<typeof clearCuttingStatusSchema>;
 const clearCuttingPointsSchema = z.array(z.number());
 export type ClearCuttingPoint = z.infer<typeof clearCuttingPointsSchema>;
 const ecologicalZoningSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  link: z.string().url(),
-  logo: z.string().url(),
+	id: z.string(),
+	name: z.string(),
+	link: z.string().url(),
+	logo: z.string().url(),
 });
 
 const clearCuttingPreviewSchema = z.object({
-  id: z.string(),
-  geoCoordinates: z.array(pointSchema),
-  center: pointSchema,
-  name: z.string().optional(),
-  cutYear: z.number(),
-  address: z.object({
-    postalCode: z.string(),
-    city: z.string(),
-    country: z.string(),
-  }),
-  imageUrl: z.string().url().optional(),
-  imagesCnt: z.number().optional(),
-  status: clearCuttingStatusSchema,
+	id: z.string(),
+	geoCoordinates: z.array(pointSchema),
+	center: pointSchema,
+	name: z.string().optional(),
+	cutYear: z.number(),
+	address: z.object({
+		postalCode: z.string(),
+		city: z.string(),
+		country: z.string(),
+	}),
+	imageUrl: z.string().url().optional(),
+	imagesCnt: z.number().optional(),
+	status: clearCuttingStatusSchema,
 });
 export type ClearCuttingPreview = z.infer<typeof clearCuttingPreviewSchema>;
 
 export const clearCuttingSchema = z.object({
-  id: z.string(),
-  geoCoordinates: z.array(pointSchema),
-  name: z.string().optional(),
-  cutYear: z.number(),
-  address: z.object({
-    streetName: z.string(),
-    streetNumber: z.string(),
-    postalCode: z.string(),
-    city: z.string(),
-    state: z.string().optional(),
-    country: z.string(),
-  }),
-  cadastralParcel: z
-    .object({
-      id: string(),
-      surfaceKm: z.number(),
-    })
-    .optional(),
-  ecologicalZonings: z.array(z.string()).optional(),
-  waterCourses: z.array(z.string()).optional(),
-  abusiveTags: z.array(z.string()).optional(),
-  customTags: z.array(z.string()).optional(),
-  imageUrls: z.array(z.string().url()),
-  status: clearCuttingStatusSchema,
+	id: z.string(),
+	geoCoordinates: z.array(pointSchema),
+	name: z.string().optional(),
+	cutYear: z.number(),
+	address: z.object({
+		streetName: z.string(),
+		streetNumber: z.string(),
+		postalCode: z.string(),
+		city: z.string(),
+		state: z.string().optional(),
+		country: z.string(),
+	}),
+	cadastralParcel: z
+		.object({
+			id: string(),
+			surfaceKm: z.number(),
+		})
+		.optional(),
+	ecologicalZonings: z.array(z.string()).optional(),
+	waterCourses: z.array(z.string()).optional(),
+	abusiveTags: z.array(z.string()).optional(),
+	customTags: z.array(z.string()).optional(),
+	imageUrls: z.array(z.string().url()),
+	status: clearCuttingStatusSchema,
 });
 
 export type ClearCutting = z.infer<typeof clearCuttingSchema>;
 
 const waterCourseSchema = z.object({
-  id: z.string(),
-  geoCoordinates: z.array(pointSchema),
+	id: z.string(),
+	geoCoordinates: z.array(pointSchema),
 });
 export const clearCuttingsResponseSchema = z.object({
-  clearCuttingsPoints: z.array(clearCuttingPointsSchema),
-  clearCuttingPreviews: z.array(clearCuttingPreviewSchema),
-  waterCourses: z.array(waterCourseSchema),
-  ecologicalZoning: z.array(ecologicalZoningSchema),
+	clearCuttingsPoints: z.array(clearCuttingPointsSchema),
+	clearCuttingPreviews: z.array(clearCuttingPreviewSchema),
+	waterCourses: z.array(waterCourseSchema),
+	ecologicalZoning: z.array(ecologicalZoningSchema),
 });
 
 export type ClearCuttingsResponse = z.infer<typeof clearCuttingsResponseSchema>;
--- frontend/src/mocks/clear-cuttings.ts
+++ frontend/src/mocks/clear-cuttings.ts
@@ -1,7 +1,7 @@
 import type {
-  ClearCuttingPreview,
-  ClearCuttingsResponse,
-  ClearCuttingStatus,
+	ClearCuttingPreview,
+	ClearCuttingsResponse,
+	ClearCuttingStatus,
 } from "@/features/clear-cutting/store/clear-cuttings";
 import type { FiltersResponse } from "@/features/clear-cutting/store/filters";
 import { range } from "@/shared/array";
@@ -10,124 +10,124 @@ import { faker } from "@faker-js/faker";
 import { http, HttpResponse } from "msw";
 
 export const mockFilters = http.get("*/filters", () => {
-  return HttpResponse.json({
-    cutYears: [2021, 2022],
-    tags: [
-      {
-        isAbusive: true,
-        name: "Supérieur à 10 hectares",
-        id: faker.string.uuid(),
-      },
-    ],
-    ecologicalZoning: {
-      [faker.string.uuid()]: faker.company.buzzAdjective(),
-    },
-    status: {},
-    departments: {},
-    region: {},
-  } satisfies FiltersResponse);
+	return HttpResponse.json({
+		cutYears: [2021, 2022],
+		tags: [
+			{
+				isAbusive: true,
+				name: "Supérieur à 10 hectares",
+				id: faker.string.uuid(),
+			},
+		],
+		ecologicalZoning: {
+			[faker.string.uuid()]: faker.company.buzzAdjective(),
+		},
+		status: {},
+		departments: {},
+		region: {},
+	} satisfies FiltersResponse);
 });
 
 export const mockClearCuttings = http.get("*/clear-cuttings", ({ request }) => {
-  const url = new URL(request.url);
-  const geoBoundsQueryString = url.searchParams.get("geoBounds");
-  let boundaries: Boundaries | undefined;
+	const url = new URL(request.url);
+	const geoBoundsQueryString = url.searchParams.get("geoBounds");
+	let boundaries: Boundaries | undefined;
 
-  if (geoBoundsQueryString) {
-    const geoBounds = geoBoundsQueryString.split(",").map(Number.parseFloat);
-    boundaries = [
-      [geoBounds[0], geoBounds[1]],
-      [geoBounds[0], geoBounds[3]],
-      [geoBounds[2], geoBounds[3]],
-      [geoBounds[2], geoBounds[1]],
-    ];
-  }
+	if (geoBoundsQueryString) {
+		const geoBounds = geoBoundsQueryString.split(",").map(Number.parseFloat);
+		boundaries = [
+			[geoBounds[0], geoBounds[1]],
+			[geoBounds[0], geoBounds[3]],
+			[geoBounds[2], geoBounds[3]],
+			[geoBounds[2], geoBounds[1]],
+		];
+	}
 
-  const clearCuttingPreviews = createFranceRandomPoints.map(createClearCutting);
+	const clearCuttingPreviews = createFranceRandomPoints.map(createClearCutting);
 
-  return HttpResponse.json({
-    clearCuttingPreviews: boundaries
-      ? clearCuttingPreviews.filter((ccp) =>
-          isPointInsidePolygon(boundaries, ccp.center)
-        )
-      : clearCuttingPreviews,
-    clearCuttingsPoints: createFranceRandomPoints,
-    ecologicalZoning: [],
-    waterCourses: [],
-  } satisfies ClearCuttingsResponse);
+	return HttpResponse.json({
+		clearCuttingPreviews: boundaries
+			? clearCuttingPreviews.filter((ccp) =>
+					isPointInsidePolygon(boundaries, ccp.center),
+				)
+			: clearCuttingPreviews,
+		clearCuttingsPoints: createFranceRandomPoints,
+		ecologicalZoning: [],
+		waterCourses: [],
+	} satisfies ClearCuttingsResponse);
 });
 
 const francRandomPointMock = (): [number, number] => [
-  faker.location.latitude({
-    min: 43.883918307385926,
-    max: 49.33292664908802,
-  }),
-  faker.location.longitude({
-    min: -0.3899356021470268,
-    max: 5.666435865557435,
-  }),
+	faker.location.latitude({
+		min: 43.883918307385926,
+		max: 49.33292664908802,
+	}),
+	faker.location.longitude({
+		min: -0.3899356021470268,
+		max: 5.666435865557435,
+	}),
 ];
 
 const randomPolygonFromLocation = (
-  point: [number, number],
-  radius = 10,
-  size = 10
+	point: [number, number],
+	radius = 10,
+	size = 10,
 ): [number, number][] => {
-  if (size < 3) {
-    throw new Error(
-      "Invalid polygon size: maximum polygon size can not be less than 3"
-    );
-  }
+	if (size < 3) {
+		throw new Error(
+			"Invalid polygon size: maximum polygon size can not be less than 3",
+		);
+	}
 
-  if (radius <= 0) {
-    throw new Error("Invalid radius size: must be grater to 0");
-  }
+	if (radius <= 0) {
+		throw new Error("Invalid radius size: must be grater to 0");
+	}
 
-  const earthRadius = 6371; // Earth radius
-  const polygon: [number, number][] = [];
-  const angleStep = (2 * Math.PI) / size;
+	const earthRadius = 6371; // Earth radius
+	const polygon: [number, number][] = [];
+	const angleStep = (2 * Math.PI) / size;
 
-  for (let i = 0; i < size; i++) {
-    const angle = i * angleStep;
+	for (let i = 0; i < size; i++) {
+		const angle = i * angleStep;
 
-    const deltaLat = (radius / earthRadius) * (180 / Math.PI) * Math.sin(angle);
-    const deltaLng =
-      ((radius / earthRadius) * (180 / Math.PI) * Math.cos(angle)) /
-      Math.cos((point[0] * Math.PI) / 180);
+		const deltaLat = (radius / earthRadius) * (180 / Math.PI) * Math.sin(angle);
+		const deltaLng =
+			((radius / earthRadius) * (180 / Math.PI) * Math.cos(angle)) /
+			Math.cos((point[0] * Math.PI) / 180);
 
-    polygon.push([point[0] + deltaLat, point[1] + deltaLng]);
-  }
+		polygon.push([point[0] + deltaLat, point[1] + deltaLng]);
+	}
 
-  return polygon;
+	return polygon;
 };
 
 const createFranceRandomPoints = range<[number, number]>(
-  500,
-  francRandomPointMock
+	500,
+	francRandomPointMock,
 );
 
 const getRandomStatus = (seed: number) =>
-  ["validated", "toValidate", "rejected", "waitingInformation"][
-    seed % 4
-  ] as ClearCuttingStatus;
+	["validated", "toValidate", "rejected", "waitingInformation"][
+		seed % 4
+	] as ClearCuttingStatus;
 
 const createClearCutting = (center: [number, number]): ClearCuttingPreview => {
-  const name = faker.animal.dog();
-  const city = faker.location.city();
+	const name = faker.animal.dog();
+	const city = faker.location.city();
 
-  return {
-    center,
-    name,
-    address: {
-      city,
-      country: faker.location.country(),
-      postalCode: faker.location.zipCode(),
-    },
-    status: getRandomStatus(Math.floor(center[0] + center[1])),
-    cutYear: 2021,
-    id: faker.string.uuid(),
-    imagesCnt: faker.number.int() % 10,
-    imageUrl: faker.image.url(),
-    geoCoordinates: randomPolygonFromLocation(center, 3.5, 7),
-  };
+	return {
+		center,
+		name,
+		address: {
+			city,
+			country: faker.location.country(),
+			postalCode: faker.location.zipCode(),
+		},
+		status: getRandomStatus(Math.floor(center[0] + center[1])),
+		cutYear: 2021,
+		id: faker.string.uuid(),
+		imagesCnt: faker.number.int() % 10,
+		imageUrl: faker.image.url(),
+		geoCoordinates: randomPolygonFromLocation(center, 3.5, 7),
+	};
 };
