0efb723c6c2397c8a2683c31ee03c7393fb09c23
Austin Flores <ctrl4ltdeleteme@gmail.com>
2025-02-25 16:16:38 +0100

    skip linting for out of order imports

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -6,9 +6,9 @@ os.environ["DATABASE_URL"] = TEST_DATABASE_URL
 # Add parent path to get access to app imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import pytest
+from sqlalchemy import create_engine  # noqa: E402
+from sqlalchemy.orm import sessionmaker  # noqa: E402
+import pytest  # noqa: E402
 from app.database import Base  # noqa: E402
 
 Base.registry.configure()
