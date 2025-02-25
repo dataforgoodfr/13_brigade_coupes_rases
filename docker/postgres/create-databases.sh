c29a9943890a878ba8f8645de1704a3be0ed46e7
Austin Flores <ctrl4ltdeleteme@gmail.com>
2025-02-25 01:05:58 +0100

    Fix tabbing. How are tabs in here?

--- docker/postgres/create-databases.sh
+++ docker/postgres/create-databases.sh
@@ -4,20 +4,20 @@ set -e
 set -u
 
 function create_user_and_database() {
-	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
-			\c $database;
-	    CREATE EXTENSION IF NOT EXISTS postgis;
+  local database=$1
+  echo "  Creating user and database '$database'"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+      CREATE DATABASE $database;
+      GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+      \c $database;
+      CREATE EXTENSION IF NOT EXISTS postgis;
 EOSQL
 }
 
 if [ -n "$DATABASES" ]; then
-	echo "Multiple database creation requested: $DATABASES"
-	for db in $(echo $DATABASES | tr ',' ' '); do
-		create_user_and_database $db
-	done
-	echo "Multiple databases created"
+  echo "Multiple database creation requested: $DATABASES"
+  for db in $(echo $DATABASES | tr ',' ' '); do
+    create_user_and_database $db
+  done
+  echo "Multiple databases created"
 fi
