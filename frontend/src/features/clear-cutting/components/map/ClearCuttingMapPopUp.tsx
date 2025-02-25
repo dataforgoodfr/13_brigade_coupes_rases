02a9675b5d394a98dcde87c11dca55c5dab1e157
Idriss <ihippocrate@gmail.com>
2025-02-25 10:10:03 +0100

    Use of the correct font

--- frontend/src/features/clear-cutting/components/map/ClearCuttingMapPopUp.tsx
+++ frontend/src/features/clear-cutting/components/map/ClearCuttingMapPopUp.tsx
@@ -6,7 +6,7 @@ export function ClearCuttingMapPopUp() {
 	return (
 		<>
 			<Popup closeButton={false} maxWidth={350} >
-				<div className="flex justify-between items-center mb-5 w-full">
+				<div className="flex justify-between items-center mb-5 w-full font-[Inter]">
 					<div className="flex items-center">
 						<h2 className="font-semibold text-lg">Nom coupe rase</h2>
 
@@ -14,12 +14,12 @@ export function ClearCuttingMapPopUp() {
 					</div>
 					<div className="text-sm">25/10/2024</div>
 				</div>
-                <div className="flex mb-5 gap-2" >
+                <div className="flex mb-5 gap-2 font-[Inter]" >
 					<Badge className="text-sm" variant="secondary">Natura 2000</Badge>
 					<Badge className="text-sm" variant="secondary">Pente > 30%</Badge>
 					<Badge className="text-sm" variant="secondary">Sup 20 HA</Badge>
 				</div>
-                <div className="flex flex-col gap-2.5 text-base text-secondary">
+                <div className="flex flex-col gap-2.5 text-base text-secondary font-[Plus Jakarta Sans] font-medium">
 					<div>Date du signalement : <strong>JJ/MM/AAA</strong></div>
 					<div>Taille de la coupe : <strong>30 HA</strong></div>
 					<div>Pente : <strong>45 %</strong></div>
