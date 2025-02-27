33da6d741fc9ddcfad0413f1e3feccb72c554b28
CindyDelage <cindy.delage@justai.co>
2025-02-27 10:52:07 +0100

    Update PyKeePass.py

--- data_pipeline/PyKeePass.py
+++ data_pipeline/PyKeePass.py
@@ -7,7 +7,7 @@ Created on Tue Feb 25 19:24:49 2025
 
 from pykeepass import PyKeePass
 
-kp = PyKeePass('secrets.kdbx', password=)
+kp = PyKeePass('secrets.kdbx', password="")
 group = kp.find_groups(name='General', first=True)
 entry = kp.find_entries(title='SCW_ACCESS_KEY', first=True)
 entry.password
