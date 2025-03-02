2a0abeea4ad7b73e7a64b3aac17c61efcc62fb9c
sbailleul <bailleul.sacha@gmail.com>
2025-03-03 00:19:07 +0100

    feat(frontend): Improve tags display

--- frontend/src/features/clear-cutting/components/list/ClearCuttingItem.tsx
+++ frontend/src/features/clear-cutting/components/list/ClearCuttingItem.tsx
@@ -1,4 +1,5 @@
 import { DotByStatus } from "@/features/clear-cutting/components/DotByStatus";
+import { TagBadge } from "@/features/clear-cutting/components/TagBadge";
 import type { ClearCuttingPreview } from "@/features/clear-cutting/store/clear-cuttings";
 import { CLEAR_CUTTING_STATUS_TRANSLATIONS } from "@/features/clear-cutting/store/status";
 import { useNavigate } from "@tanstack/react-router";
@@ -8,6 +9,7 @@ export function ClearCuttingItem({
 	reportDate,
 	status,
 	comment,
+	abusiveTags,
 	address: { city },
 }: ClearCuttingPreview) {
 	const navigate = useNavigate();
@@ -26,7 +28,7 @@ export function ClearCuttingItem({
 					handleCardClick();
 				}
 			}}
-			className="flex flex-col cursor-pointer"
+			className="flex flex-col cursor-pointer gap-2"
 		>
 			<div className="flex justify-between">
 				<h3 className="me-auto text-lg font-bold text-gray-800">{city}</h3>
@@ -36,12 +38,12 @@ export function ClearCuttingItem({
 				<DotByStatus status={status} />
 				{CLEAR_CUTTING_STATUS_TRANSLATIONS[status]}
 			</div>
-			{comment && (
-				<p className="text-zinc-500 line-clamp-2">
-					{comment}
-					{comment}
-				</p>
-			)}
+			{comment && <p className="text-zinc-500 line-clamp-2">{comment}</p>}
+			<div className="flex gap-2 ">
+				{abusiveTags.map((tag) => (
+					<TagBadge key={tag.id} {...tag} />
+				))}
+			</div>
 		</li>
 	);
 }
