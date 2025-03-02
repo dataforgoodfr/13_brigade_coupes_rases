1b709b542f4dfb12a04ba3b2facfe6d400faad29
inimaz <93inigo93@gmail.com>
2025-03-02 11:54:37 +0100

    add ENVIRONMENT=TEST env var for unit tests
    Signed-off-by: inimaz <93inigo93@gmail.com>

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -3,6 +3,7 @@ import sys
 
 TEST_DATABASE_URL = "postgresql://devuser:devuser@db:5432/local"
 os.environ["DATABASE_URL"] = TEST_DATABASE_URL
+os.environ["ENVIRONMENT"] = "TEST"
 # Add parent path to get access to app imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
