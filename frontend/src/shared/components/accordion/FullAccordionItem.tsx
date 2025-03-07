2e3ba16eb6949c5910f454c61a779c19894a1efa
Esr0g <gorse.sacha@gmail.com>
2025-03-07 16:48:31 +0100

    feat(clear-cutting-form): First implementation of the clear cutting form

--- frontend/src/shared/components/accordion/FullAccordionItem.tsx
+++ frontend/src/shared/components/accordion/FullAccordionItem.tsx
@@ -4,14 +4,17 @@ import {
 	AccordionTrigger,
 } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import React from "react";
 
 type AccordionFullItemProps = {
 	title: string;
 	children?: React.ReactNode;
 	className?: string;
+	className?: string;
 };
 
+export function AccordionFullItem({ title, children, className }: AccordionFullItemProps) {
 export function AccordionFullItem({
 	title,
 	children,
@@ -23,8 +26,15 @@ export function AccordionFullItem({
 		<>
 			<AccordionItem
 				value={val}
+				className={cn(
+							"data-[state=closed]:border-b-2 data-[state=closed]:border-(--border) data-[state=open]:border-b-0 text-(--color-primary)",
+							className,
+						)}
 				className="data-[state=closed]:border-b-1 data-[state=closed]:border-(--border) data-[state=open]:border-b-0 overflow-hidden"
 			>
+				<AccordionTrigger className="cursor-pointer text-2xl">{title}</AccordionTrigger>
+				<AccordionContent className="text-black">
+					{children}
 				<AccordionTrigger className="cursor-pointer text-lg font-bold font-[Roboto]">
 					{title}
 				</AccordionTrigger>
