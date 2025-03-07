60fced5604e52d8a4aeaeeb9ac374eb43382484c
vaCalafell <calafellvalentin@yahoo.fr>
2025-03-07 08:57:26 +0100

    Remove commented code
    Co-authored-by: Pascal Le Merrer <873398+PascalLeMerrer@users.noreply.github.com>

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -17,12 +17,6 @@ def create_new_user(item: UserCreate, db: Session = db_dependency) -> UserRespon
     return create_user(db, item)
 
 
-# @router.patch("/{id}", response_model=ClearCutResponse, status_code=201)
-# def update_existing_clearcut(id: int, item: ClearCutPatch, db: Session = db_dependency) -> ClearCutResponse:
-#     logger.info(db)
-#     return update_clearcut(id, db, item)
-
-
 @router.get("/", response_model=list[UserResponse])
 def list_users(db: Session = db_dependency, skip: int = 0, limit: int = 10) -> list[UserResponse]:
     logger.info(db)
