52754d7f31813528d07d903f68f5fd2db24e1911
Valentin Calafell <calafell@stilogsud.local>
2025-03-07 14:31:40 +0100

    remove alembic for pytest

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -2,19 +2,18 @@ import os
 import sys
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine  # noqa: E402
+from sqlalchemy import create_engine, text  # noqa: E402
 from sqlalchemy.orm import sessionmaker  # noqa: E402
 
 TEST_DATABASE_URL = "postgresql://devuser:devuser@db:5432/test"
 os.environ["DATABASE_URL"] = TEST_DATABASE_URL
 os.environ["ENVIRONMENT"] = "TEST"
 from alembic.config import Config  # noqa: E402
-from alembic import command  # noqa: E402
 
 # Add parent path to get access to app imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.main import app  # noqa: E402
-from app.database import get_db  # noqa: E402
+from app.database import Base, get_db  # noqa: E402
 
 
 engine = create_engine(
@@ -28,14 +27,18 @@ alembic_cfg = Config("alembic.ini")
 
 @pytest.fixture(scope="session")
 def db():
-    command.upgrade(alembic_cfg, "head")
+    with engine.begin() as connection: 
+        connection.execute(text("CREATE EXTENSION IF NOT EXISTS postgis;"))
+
+    Base.metadata.create_all(bind=engine)
+
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.rollback()
         db.close()
-    command.downgrade(alembic_cfg, "base")
+    Base.metadata.drop_all(bind=engine)
 
 
 @pytest.fixture(scope="function")
