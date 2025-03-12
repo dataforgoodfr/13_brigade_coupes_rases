87662396f8f38314d0784d89d3948fa4eac31b2e
Esr0g <gorse.sacha@gmail.com>
2025-03-12 18:00:19 +0100

    feat(clear-cutting-form): Implementation of a first version of the form and cleanup files

--- frontend/src/features/clear-cutting/store/clear-cuttings-slice.ts
+++ frontend/src/features/clear-cutting/store/clear-cuttings-slice.ts
@@ -130,5 +130,4 @@ export const useGetClearCutting = (id: string) => {
 	useEffect(() => {
 		dispatch(getClearCuttingThunk(id));
 	}, [id, dispatch]);
-	return useAppSelector(selectDetail);
 };
--- frontend/src/shared/components/form/FormToggleGroup.tsx
+++ frontend/src/shared/components/form/FormToggleGroup.tsx
@@ -1,30 +1,61 @@
-import { FieldValues } from "react-hook-form";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage, FormProps } from "./Form";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import type { FieldValues } from "react-hook-form";
+import {
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	type FormProps,
+} from "./Form";
 
-export function FormToggleGroup<T extends FieldValues = FieldValues>({control, name, label}: FormProps<T>) {
-	
+export function FormToggleGroup<T extends FieldValues = FieldValues>({
+	control,
+	name,
+	label,
+	disabled = false,
+}: FormProps<T>) {
+	const toggleGroupItemClass =
+		"data-[state=on]:bg-(--color-primary) data-[state=on]:text-(--color-primary-foreground)";
+
 	return (
 		<FormField
 			control={control}
 			name={name}
-			render={({ field }) =>{ console.log(field);
-				return(
-
-				<FormItem>
-					<FormLabel>{label}</FormLabel>
-					<FormControl>
-						<ToggleGroup type="single" defaultValue={field.value + ""} onValueChange={(newValue) => {
-								if (newValue) 
-									return field.onChange(newValue)
-							}}>
-							<ToggleGroupItem value="true">Oui</ToggleGroupItem>
-							<ToggleGroupItem value="false">Non</ToggleGroupItem>
-							<ToggleGroupItem value="null">Pas d'informations</ToggleGroupItem>
-						</ToggleGroup>
-					</FormControl>
-					<FormMessage />
-				</FormItem>	
-			)}}
-		/>);
+			render={({ field }) => {
+				return (
+					<FormItem>
+						{label && (
+							<FormLabel className="font-bold block">{label}</FormLabel>
+						)}
+						<FormControl>
+							<ToggleGroup
+								className="shadow-[0px_2px_6px_0px_#00000033] max-w-fit ml-1 my-2"
+								type="single"
+								disabled={disabled}
+								value={`${field.value}`}
+								onValueChange={(newValue) => {
+									if (newValue) {
+										return field.onChange(newValue);
+									}
+									return;
+								}}
+							>
+								<ToggleGroupItem className={toggleGroupItemClass} value="true">
+									Oui
+								</ToggleGroupItem>
+								<ToggleGroupItem className={toggleGroupItemClass} value="false">
+									Non
+								</ToggleGroupItem>
+								<ToggleGroupItem className={toggleGroupItemClass} value="null">
+									Pas d'informations
+								</ToggleGroupItem>
+							</ToggleGroup>
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				);
+			}}
+		/>
+	);
 }
