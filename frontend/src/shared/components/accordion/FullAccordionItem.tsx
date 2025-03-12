58db18820641634ed97f377fd1b386ffec244b05
Esr0g <gorse.sacha@gmail.com>
2025-03-12 18:00:19 +0100

    feat(clear-cutting-form): Implementation of a first version of the form and cleanup files

--- frontend/src/shared/components/accordion/FullAccordionItem.tsx
+++ frontend/src/shared/components/accordion/FullAccordionItem.tsx
@@ -4,17 +4,14 @@ import {
 	AccordionTrigger,
 } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
-import { cn } from "@/lib/utils";
 import React from "react";
 
 type AccordionFullItemProps = {
 	title: string;
 	children?: React.ReactNode;
 	className?: string;
-	className?: string;
 };
 
-export function AccordionFullItem({ title, children, className }: AccordionFullItemProps) {
 export function AccordionFullItem({
 	title,
 	children,
@@ -32,8 +29,15 @@ export function AccordionFullItem({
 						)}
 				className="data-[state=closed]:border-b-1 data-[state=closed]:border-(--border) data-[state=open]:border-b-0 overflow-hidden"
 			>
-				<AccordionTrigger className="cursor-pointer text-2xl">{title}</AccordionTrigger>
-				<AccordionContent className="text-black">
+				<AccordionTrigger className="cursor-pointer text-lg font-bold font-[Roboto]">
+					{title}
+				</AccordionTrigger>
+				<AccordionContent
+					className={cn(
+						"font-[Roboto] border-t-1 pt-4 overflow-hidden",
+						className,
+					)}
+				>
 					{children}
 				<AccordionTrigger className="cursor-pointer text-lg font-bold font-[Roboto]">
 					{title}
