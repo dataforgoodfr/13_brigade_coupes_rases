1bd289d837e34a0465ed91fb3e96135b7fe9c702
sbailleul <bailleul.sacha@gmail.com>
2025-03-15 00:28:09 +0100

    feat(layers): Improve layers

--- frontend/src/features/clear-cutting/components/map/ClearCuttings.tsx
+++ frontend/src/features/clear-cutting/components/map/ClearCuttings.tsx
@@ -2,14 +2,15 @@ import { DISPLAY_PREVIEW_ZOOM_LEVEL } from "@/features/clear-cutting/store/clear
 import { selectClearCuttings } from "@/features/clear-cutting/store/clear-cuttings-slice";
 import { setGeoBounds } from "@/features/clear-cutting/store/filters.slice";
 import { CLEAR_CUTTING_STATUS_COLORS } from "@/features/clear-cutting/store/status";
+import { ToggleGroup } from "@/shared/components/toggle-group/ToggleGroup";
 import { useGeolocation } from "@/shared/hooks/geolocation";
 import { useAppDispatch, useAppSelector } from "@/shared/hooks/store";
+import { type SelectableItemEnhanced, useSingleSelect } from "@/shared/items";
 import type { ZoomAnimEventHandlerFn } from "leaflet";
+import * as L from "leaflet";
 import { useCallback, useEffect, useState } from "react";
 import { Circle, Polygon, useMap, useMapEvents } from "react-leaflet";
 import { ClearCuttingMapPopUp } from "./ClearCuttingMapPopUp";
-import * as L from "leaflet";
-import ButtonGroup from "@/shared/components/button/ButtonGroup";
 
 export function ClearCuttings() {
 	const map = useMap();
@@ -17,34 +18,52 @@ export function ClearCuttings() {
 	const [displayClearCuttingPreview, setDisplayClearCuttingPreview] =
 		useState(false);
 
+	const [layer, layers, setLayer] = useSingleSelect<
+		L.TileLayer,
+		SelectableItemEnhanced<L.TileLayer>
+	>([
+		{
+			isSelected: true,
+			item: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				id: "OpenStreetMap.Mapnik",
+				attribution:
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}),
+			label: "Standard",
+			value: "standard",
+		},
+		{
+			isSelected: false,
+			item: L.tileLayer("https://b.tile.opentopomap.org/{z}/{x}/{y}.png", {
+				id: "OpenTopoMap",
+			}),
+			label: "Terrain",
+			value: "ground",
+		},
+		{
+			isSelected: false,
+			item: L.tileLayer(
+				"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+				{
+					id: "ArcGIS.WorldImagery",
+				},
+			),
+			label: "Satellite",
+			value: "satellite",
+		},
+	]);
 	map.attributionControl.setPosition("bottomleft");
 
-	const layers = {
-		Standard: L.tileLayer(
-			"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-			{
-				id: "OpenStreetMap.Mapnik",
-				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-			},
-		),
-		Terrain: L.tileLayer("https://b.tile.opentopomap.org/{z}/{x}/{y}.png", {
-			id: "OpenTopoMap",
-
-		}),
-		Satellite: L.tileLayer(
-			"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-			{
-				id: "ArcGIS.WorldImagery",
-			},
-		),
-	} as Record<string, L.TileLayer>;
-
-	const onLayerSelected = (current: string, previous: string) => {
-		const previousLayer = layers[previous];
-		const currentLayer = layers[current];
-
-		map.removeLayer(previousLayer);
-		map.addLayer(currentLayer);
+	const handleLayerSelected = (
+		selectableItem: SelectableItemEnhanced<L.TileLayer> | undefined,
+	) => {
+		if (layer?.item) {
+			map.removeLayer(layer?.item);
+		}
+		if (selectableItem?.item) {
+			map.addLayer(selectableItem.item);
+		}
+		setLayer(selectableItem);
 	};
 
 	useEffect(() => {
@@ -132,8 +151,16 @@ export function ClearCuttings() {
 
 	return (
 		<>
-			<div className="absolute bottom-5 right-5">
-				<ButtonGroup options={Object.keys(layers)} onSelect={onLayerSelected} />
+			<div className="leaflet-bottom leaflet-right">
+				<div className="leaflet-control bg-zinc-100 p-1 rounded-md ">
+					<ToggleGroup
+						variant="primary"
+						type="single"
+						size="xl"
+						value={layers}
+						onValueChange={handleLayerSelected}
+					/>
+				</div>
 			</div>
 
 			<ClearCuttingPreview />
