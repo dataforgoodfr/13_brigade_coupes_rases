3b212e03a0055bed60ef93dcef8daa12c700e633
Cindy Delage <cindy.delage@justai.co>
2025-03-23 14:26:04 +0100

    fix(pre-commit) : fix pre commit bug

--- data_pipeline/scripts/utils/s3.py
+++ data_pipeline/scripts/utils/s3.py
@@ -3,32 +3,26 @@ import boto3
 from dotenv import load_dotenv
 from pykeepass import PyKeePass
 
+
 class S3Manager:
     def __init__(self):
         load_dotenv()
-
         keepass_password = os.getenv("KEEPASS_PASSWORD")
-
         if keepass_password is None:
             raise ValueError("La variable d'environnement KEEPASS_PASSWORD n'est pas définie.")
-
         kp = PyKeePass(
             "../../../keepass/secrets.kdbx", password=keepass_password
         )  # Accès à la base KeePass
-
         entry = kp.find_entries(
             title="SCW_ACCESS_KEY", first=True
         )  # Récupérer la clé d'accès du bucket S3
         access_key = entry.password if entry else None
-
         entry = kp.find_entries(
             title="SCW_SECRET_KEY", first=True
         )  # Récupérer la clé secrète du bucket S3
         secret_key = entry.password if entry else None
-
         if not access_key or not secret_key:
             raise ValueError("Impossible de récupérer les credentials S3 depuis KeePass.")
-
         print("Access Key et Secret Key récupérés avec succès.")
         kwargs = {
             "service_name": "s3",
@@ -39,7 +33,6 @@ class S3Manager:
         endpoint = os.getenv("S3_ENDPOINT")
         if endpoint:
             kwargs["endpoint_url"] = endpoint
-
         self.s3 = boto3.client(**kwargs)
         self.bucket_name = os.getenv("S3_BUCKET_NAME")
 
