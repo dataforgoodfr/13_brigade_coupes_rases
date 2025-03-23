579407fb3c286a2ba8b4726f76d12d05355fdc5b
Cindy Delage <cindy.delage@justai.co>
2025-03-23 13:57:15 +0100

    fix(pre-commit) : fix pre commit bug

---
+++ data_pipeline/scripts/utils/logging_etl.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def etl_logger(log_filename):
+    logging.basicConfig(
+        filename=log_filename,
+        filemode="a",
+        format="%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        # level=logging.DEBUG
+        level=logging.INFO,
+    )
+
+    return logging.getLogger(__name__)
