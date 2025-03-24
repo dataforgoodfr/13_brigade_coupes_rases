48b894243b6427038b2d6b8d5bf71c1da5d0f5e3
sbailleul <bailleul.sacha@gmail.com>
2025-03-25 00:39:31 +0100

    fix(CI): Fix builds and precommit checks

--- backend/app/routes/me.py
+++ backend/app/routes/me.py
@@ -1,19 +1,12 @@
-from logging import getLogger
-from fastapi import APIRouter, Depends, logger
-from pytest import Session
+from fastapi import APIRouter, Depends
 from app.models import User
 from app.schemas.user import UserResponse
 from app.services.user import map_user
 from app.services.user_auth import get_current_user
-from app.deps import db_session
 
 router = APIRouter(prefix="/api/v1/me", tags=["Me"])
-logger = getLogger(__name__)
 
 
 @router.get("/", response_model=UserResponse, status_code=200)
-def get_me(
-    db: Session = db_session, user: User = Depends(get_current_user)
-) -> UserResponse:
-    logger.info(db)
+def get_me(user: User = Depends(get_current_user)) -> UserResponse:
     return map_user(user)
