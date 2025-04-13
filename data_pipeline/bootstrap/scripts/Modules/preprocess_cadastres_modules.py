2c4e3b3c8245f338d0d3694d20fd571e91c4cbe9
Cindy Delage <cindy.delage@justai.co>
2025-04-13 15:49:10 +0200

    chore/modules : convert functions to modules

---
+++ data_pipeline/bootstrap/scripts/Modules/preprocess_cadastres_modules.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 12 20:20:22 2025
+
+@author: cindy
+"""
+
+import logging
+import gzip
+import shutil
+
+import geopandas as gpd
+
+from scripts import DATA_DIR
+from scripts.utils import display_df, download_file, load_gdf
+
+CADASTRE_DEPARTMENTS_DIR = DATA_DIR / "cadastre_departments"
+RESULT_FILEPATH = CADASTRE_DEPARTMENTS_DIR / "cadastre_departments.fgb"
+
+def download_cadastre(cad_type, url, cad_dir) -> gpd.GeoDataFrame:
+    logging.info(f"Downloading the cadastre for {type}")
+
+    download_file(
+        url,
+        cad_dir,
+    )
+
+    result = load_gdf(cad_dir)
+
+    return result
+
+def unzip(gz_file,json_file):
+    logging.info("Unzipping the gzipped json")
+    with gzip.open(gz_file, "rb") as f_in:
+        with open(json_file, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+    result = load_gdf(json_file)
+    return result 
+
+def remove_overseas(cad_type: gpd.GeoDataFrame,col,listed) -> gpd.GeoDataFrame:
+    logging.info(f"Removing the overseas {type}")
+    if cad_type == "departments":
+        cad_type = cad_type[
+            ~cad_type[col].isin(listed)
+        ]
+    else:
+        cad_type = cad_type[~cad_type[col].str.startswith(listed)]
+    display_df(cad_type)
+    return cad_type
+
+def convert_crs_to_lambert93(cad_type: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    logging.info("Convert CRS to Lambert93 like the other project layers")
+    cad_type = cad_type.to_crs(epsg=2154)
+    return cad_type
---
+++ data_pipeline/bootstrap/scripts/Modules/preprocess_natura2000_modules.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 13 15:34:05 2025
+
+@author: cindy
+"""
+
+import logging
+
+import geopandas as gpd
+
+from scripts.utils import display_df, download_file, load_gdf
+
+def download_file_module(url,output_dir) -> None:
+    logging.info("Starting download")
+    download_file(url, output_dir)
+    logging.info("Download complete")
+    return
+
+
+def load_layers(input_dir) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    logging.info("Loading the layers")
+    load = load_gdf(input_dir)
+    logging.info("Loaded successfully")
+    return load
+
+def union_module(input_file: gpd.GeoDataFrame,crs) -> gpd.GeoDataFrame:
+    logging.info("Starts unioning")
+    union = gpd.GeoDataFrame(
+        geometry=[input_file.union_all()], crs=crs
+    )
+    display_df(union)
+    return union
