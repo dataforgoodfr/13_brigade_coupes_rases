781afb842b08deb7eab72c611cd1b2e8c90e3ad1
Yoann CROUIN <ycrouin@talend.com>
2025-04-13 20:03:18 +0200

    Add preprocess_bdforet

--- data_pipeline/bootstrap/scripts/run_pipeline.py
+++ data_pipeline/bootstrap/scripts/run_pipeline.py
@@ -1,6 +1,6 @@
 from scripts.enrich_sufosat_clusters import enrich_sufosat_clusters
+from scripts.preprocess_bdforet import preprocess_bdforet
 from scripts.preprocess_cadastre_cities import preprocess_cadastre_cities
-from scripts.preprocess_cadastre_departments import preprocess_cadastre_departments
 from scripts.preprocess_natura2000 import preprocess_natura2000
 from scripts.preprocess_slope import preprocess_slope
 from scripts.preprocess_sufosat import preprocess_sufosat
@@ -8,9 +8,9 @@ from scripts.preprocess_sufosat import preprocess_sufosat
 
 def run_pipeline() -> None:
     preprocess_sufosat()
+    preprocess_bdforet()
     preprocess_slope()
     preprocess_natura2000()
-    preprocess_cadastre_departments()
     preprocess_cadastre_cities()
     enrich_sufosat_clusters()
 
