24420d89d6bb9799687e8cfc4c104d8fd4e7018d
Cindy Delage <cindy.delage@justai.co>
2025-04-13 15:55:10 +0200

    fix pre-commit error

--- data_pipeline/bootstrap/scripts/Modules/preprocess_cadastres_modules.py
+++ data_pipeline/bootstrap/scripts/Modules/preprocess_cadastres_modules.py
@@ -5,6 +5,13 @@ Created on Sat Apr 12 20:20:22 2025
 @author: cindy
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 12 20:20:22 2025
+
+@author: cindy
+"""
+
 import logging
 import gzip
 import shutil
@@ -17,8 +24,9 @@ from scripts.utils import display_df, download_file, load_gdf
 CADASTRE_DEPARTMENTS_DIR = DATA_DIR / "cadastre_departments"
 RESULT_FILEPATH = CADASTRE_DEPARTMENTS_DIR / "cadastre_departments.fgb"
 
+
 def download_cadastre(cad_type, url, cad_dir) -> gpd.GeoDataFrame:
-    logging.info(f"Downloading the cadastre for {type}")
+    logging.info(f"Downloading the cadastre for {cad_type}")
 
     download_file(
         url,
@@ -29,27 +37,29 @@ def download_cadastre(cad_type, url, cad_dir) -> gpd.GeoDataFrame:
 
     return result
 
-def unzip(gz_file,json_file):
+
+def unzip(gz_file, json_file):
     logging.info("Unzipping the gzipped json")
     with gzip.open(gz_file, "rb") as f_in:
         with open(json_file, "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
     result = load_gdf(json_file)
-    return result 
+    return result
 
-def remove_overseas(cad_type: gpd.GeoDataFrame,col,listed) -> gpd.GeoDataFrame:
-    logging.info(f"Removing the overseas {type}")
+
+def remove_overseas(cad_type: gpd.GeoDataFrame, col, listed) -> gpd.GeoDataFrame:
+    logging.info(f"Removing the overseas {cad_type}")
     if cad_type == "departments":
-        cad_type = cad_type[
-            ~cad_type[col].isin(listed)
-        ]
+        cad_type = cad_type[~cad_type[col].isin(listed)]
     else:
         cad_type = cad_type[~cad_type[col].str.startswith(listed)]
     display_df(cad_type)
     return cad_type
 
+
 def convert_crs_to_lambert93(cad_type: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     logging.info("Convert CRS to Lambert93 like the other project layers")
     cad_type = cad_type.to_crs(epsg=2154)
     return cad_type
+
