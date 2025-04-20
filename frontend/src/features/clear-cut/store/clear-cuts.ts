005811f8ab2102fffb4e3477d232751e2bd9a5a9
Yoann CROUIN <ycrouin@talend.com>
2025-04-20 14:51:26 +0200

    Modify frontend popup

--- frontend/src/features/clear-cut/store/clear-cuts.ts
+++ frontend/src/features/clear-cut/store/clear-cuts.ts
@@ -66,6 +66,10 @@ export const clearCutReportResponseSchema = z.object({
 	created_at: z.string().date(),
 	updated_at: z.string().date(),
 	total_area_hectare: z.number(),
+	bdf_resinous_area_hectare: z.number(),
+	bdf_deciduous_area_hectare: z.number(),
+	bdf_mixed_area_hectare: z.number(),
+	bdf_poplar_area_hectare: z.number(),
 	last_cut_date: z.string().date(),
 	rules_ids: z.array(z.string()),
 });
