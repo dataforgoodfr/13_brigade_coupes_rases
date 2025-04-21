98719ecabcdd27c986d97ff73c25061106784d76
Cindy Delage <cindy.delage@justai.co>
2025-04-21 14:10:44 +0200

    Update preprocess.py

--- data_pipeline/bootstrap/scripts/preprocess.py
+++ data_pipeline/bootstrap/scripts/preprocess.py
@@ -1,8 +1,8 @@
-from script.modules.cities import preprocess_cadastre_cities
-from script.modules.departments import preprocess_cadastre_departments
-from script.modules.natura2000 import preprocess_natura2000
-from script.modules.slope import preprocess_slope
-from script.modules.sufosat import preprocess_sufosat
+from scripts.modules.cities import preprocess_cadastre_cities
+from scripts.modules.departments import preprocess_cadastre_departments
+from scripts.modules.natura2000 import preprocess_natura2000
+from scripts.modules.slope import preprocess_slope
+from scripts.modules.sufosat import preprocess_sufosat
 
     
 if __name__ == "__main__":
