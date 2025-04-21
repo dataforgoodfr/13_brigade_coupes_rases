ed838513c7c7a250748627a77d583775d925594c
Cindy Delage <cindy.delage@justai.co>
2025-04-21 14:16:22 +0200

    Update __init__.py

--- data_pipeline/bootstrap/scripts/modules/__init__.py
+++ data_pipeline/bootstrap/scripts/modules/__init__.py
@@ -3,7 +3,7 @@ from scripts.utils.disjoint_set import DisjointSet  # type: ignore
 from scripts.utils.download_file import download_file
 from scripts.utils.log_execution import log_execution
 from scripts.utils.polygonize_raster import polygonize_raster
-from script.utils.overlay import overlay
+from scripts.utils.overlay import overlay
 
 __all__ = [
     "DisjointSet",
