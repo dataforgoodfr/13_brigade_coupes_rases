30d918660e970eadab1c36899f8ccf6cb6d839e0
Yoann CROUIN <ycrouin@talend.com>
2025-07-10 13:26:26 +0200

    fix(): Add default values to config for the database upgrade CI to work

--- backend/app/config.py
+++ backend/app/config.py
@@ -32,26 +32,32 @@ class Settings(BaseSettings):
         default="", json_schema_extra={"env": "IMPORTS_TOKEN", "secret": True}
     )
     S3_ACCESS_KEY_ID: str = Field(
+        default="",
         json_schema_extra={"env": "S3_ACCESS_KEY", "secret": True},
         description="Access key ID for the S3 bucket for photos storage",
     )
     S3_SECRET_ACCESS_KEY: str = Field(
+        default="",
         json_schema_extra={"env": "S3_SECRET_ACCESS_KEY", "secret": True},
         description="Secret access key for the S3 bucket for photos storage",
     )
     S3_BUCKET_NAME: str = Field(
+        default="",
         json_schema_extra={"env": "S3_BUCKET_NAME"},
         description="Name of the S3 bucket for photos storage",
     )
     S3_PREFIX: str = Field(
+        default="",
         json_schema_extra={"env": "S3_PREFIX"},
         description="Prefix for the S3 bucket for photos storage, useful to organize photos in subfolders and to differentiate between different environments (e.g., 'development/', 'production/')",
     )
     S3_REGION: str = Field(
+        default="",
         json_schema_extra={"env": "S3_REGION"},
         description="Region of the S3 bucket for photos storage",
     )
     S3_ENDPOINT: str = Field(
+        default="",
         json_schema_extra={"env": "S3_ENDPOINT"},
         description="Endpoint URL of the S3 bucket for photos storage",
     )
