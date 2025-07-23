440ac290387acb81bde5420787a361bd57414ab2
sbailleul <bailleul.sacha@gmail.com>
2025-07-23 08:12:40 +0200

    chore(merge): Merge changes

---
+++ frontend/src/shared/components/form/FormInput.tsx
@@ -0,0 +1,36 @@
+import { Input } from "@/components/ui/input";
+import type { HTMLInputTypeAttribute } from "react";
+import type { FieldValues } from "react-hook-form";
+import {
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	type FormProps,
+} from "./Form";
+
+export function FormInput<T extends FieldValues = FieldValues>({
+	control,
+	name,
+	label,
+	type,
+	disabled = false,
+	...props
+}: FormProps<T> & { type: HTMLInputTypeAttribute }) {
+	return (
+		<FormField
+			control={control}
+			name={name}
+			render={({ field }) => (
+				<FormItem className="flex gap-4 items-center">
+					{label && <FormLabel className="font-bold">{label}</FormLabel>}
+					<FormControl>
+						<Input type={type} disabled={disabled} {...field} {...props} />
+					</FormControl>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+}
--- frontend/src/test/features/administration.test.tsx
+++ frontend/src/test/features/administration.test.tsx
@@ -3,10 +3,10 @@ import { renderApp } from "@/test/renderApp";
 import { screen } from "@testing-library/react";
 import { describe, it } from "vitest";
 
-describe("Users list", () => {
-	it("should see users list if administrator", async () => {
+describe("Administration", () => {
+	it("should see administration button if administrator", async () => {
 		const { user } = renderApp({ route: "/login" });
 		await loginForm({ user }).logAdministrator();
-		await user.click(await screen.findByTitle("Utilisateurs"));
+		await user.click(await screen.findByTitle("Administration"));
 	});
 });
