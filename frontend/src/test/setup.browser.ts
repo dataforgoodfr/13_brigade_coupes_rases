a1831d6caca5206db482ffc6c0618ae386155ecf
sbailleul <bailleul.sacha@gmail.com>
2025-08-08 13:59:14 +0200

    fix(test): Fix test

--- frontend/src/test/setup.browser.ts
+++ frontend/src/test/setup.browser.ts
@@ -1,10 +1,9 @@
 import { worker } from "@/mocks/browser";
 import "@testing-library/jest-dom/vitest";
 import { configure } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll, beforeEach, vi } from "vitest";
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
 
 import "../index.css";
-import { cleanup } from "vitest-browser-react";
 
 import.meta.glob("../../dist/assets/*.css", { eager: true });
 
@@ -16,7 +15,6 @@ configure({
 	asyncUtilTimeout: 5_000,
 });
 
-beforeEach(() => cleanup());
 beforeAll(() => worker.start());
 afterEach(() => {
 	worker.resetHandlers();
