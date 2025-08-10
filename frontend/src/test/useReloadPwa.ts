824508dedcd3722e98bde43d7b07d1975a41f2ab
sbailleul <bailleul.sacha@gmail.com>
2025-08-10 16:03:49 +0200

    fix import

--- frontend/src/test/useReloadPwa.ts
+++ frontend/src/test/useReloadPwa.ts
@@ -1,4 +1,6 @@
 export function useReloadPwa() {
 	// biome-ignore lint/suspicious/noConsole: Test purpose
-	console.log("This function replace original hook in test environment cause of vite dependency optimizer error");
+	console.log(
+		"This function replace original hook in test environment cause of vite dependency optimizer error",
+	);
 }
